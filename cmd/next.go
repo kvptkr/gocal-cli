efa9671522ce97abbffbaca03f0d41508c277d25
Kanav Petkar <k.petkar21@gmail.comm>
2020-01-01 23:19:50 -0500

    added first commands

---
+++ cmd/next.go
@@ -0,0 +1,70 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"google.golang.org/api/calendar/v3"
+	"log"
+	"time"
+	"github.com/kvptkr/gocal-cli/pkg/auth"
+	"github.com/spf13/cobra"
+)
+
+// nextCmd represents the next command
+var nextCmd = &cobra.Command{
+	Use:   "next",
+	Short: "Displays the next ten events",
+	Long: `Displays the next ten events on your calendar, regardless of what calendar they might appear on`,
+	Run: func(cmd *cobra.Command, args []string) {
+		srv := auth.GetClient()
+		getNextTenEvents(srv)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(nextCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// nextCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// nextCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+func getNextTenEvents(calendar *calendar.Service) {
+	t := time.Now().Format(time.RFC3339)
+	events, err := calendar.Events.List("primary").ShowDeleted(false).
+		SingleEvents(true).TimeMin(t).MaxResults(10).OrderBy("startTime").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+	}
+	fmt.Println("Next 10 upcoming events:")
+	if len(events.Items) == 0 {
+		fmt.Println("No upcoming events found.")
+	} else {
+		for _, item := range events.Items {
+			date := item.Start.DateTime
+			if date == "" {
+				date = item.Start.Date
+			}
+			fmt.Printf("%v (%v)\n", item.Summary, date)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/kvptkr/gocal-cli
+
+go 1.13
+
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.6.1
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	google.golang.org/api v0.15.0
+)
